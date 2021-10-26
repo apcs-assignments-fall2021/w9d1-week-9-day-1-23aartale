7a0c193e3b1737a8fc16e0b4f17c78543f6e53e7
adrianartale <23aartale@athenian.org>
2021-10-26 10:38:40 -0700

    this commit is fire

--- src/main/java/BankAccount.java
+++ src/main/java/BankAccount.java
@@ -3,4 +3,39 @@ public class BankAccount {
     private int balance;
     private int accountNumber;
     private String preferredName;
+
+    //Constructor
+    public BankAccount(int b, int a, String p){
+        balance = b;
+        accountNumber = a;
+        preferredName = p;
+    }
+
+    public void withdraw(int x){
+        balance -= x;
+    }
+
+    public void deposit(int x){
+        balance += x;
+    }
+
+    public int getBalance(){
+        return balance;
+    }
+
+    public int getAccountNumber(){
+        return accountNumber;
+    }
+
+    public String getPreferredName(){
+        return preferredName;
+    }
+
+    public void setPreferredName(String name){
+        preferredName = name;
+    }
+
+    public String toString(){
+        return "Your account number is " + accountNumber + "\n Your balance is " + balance;
+    }
 }
--- src/main/java/BankClient.java
+++ src/main/java/BankClient.java
@@ -4,9 +4,44 @@ public class BankClient {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("What is your preferred name?");
+        System.out.println("What is your preferred name? ");
         String name = scan.nextLine();
+        BankAccount account = new BankAccount(0, 101134, name);
+        while (true) {
+            System.out.println("Hi " + name + "! What command would you like to carry out? ");
+            System.out.println("(withdraw/deposit/check balance/check account number/set preferred name/print summary)");
+            String choice = scan.nextLine();
+            if (choice.equals("withdraw")) {
+                System.out.println("how much do you want to withdraw?");
+                int x = scan.nextInt();
+                account.withdraw(x);
+            }
+
+            else if (choice.equals("deposit")) {
+                System.out.println("How much do you want to deposit?");
+                int x = scan.nextInt();
+                account.deposit(x);
+            }
+
+            else if (choice.equals("check balance")) {
+                System.out.println(account.getBalance());
+            }
+
+            else if (choice.equals("check account number")) {
+                System.out.println(account.getAccountNumber());
+            }
+
+            else if (choice.equals("set preferred name")) {
+                System.out.println("What do you want to set it too?");
+                name = scan.nextLine();
+                account.setPreferredName(name);
+            }
+
+            else if (choice.equals("print summary")) {
+                System.out.println(account.toString());
+            }
 //        // Create a bank account with the given name and the account number 101134
 //        BankAccount account = new BankAccount(name, 101134);
+        }
     }
 }
